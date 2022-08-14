a4a4053cbce25bd9cdf3b4aeb3ed1ff2dc28958b
Nathaniel C. Hall <nathaniel.hall@gmail.com>
2022-08-14 11:26:44 -0400

    new client side project

---
+++ client2/webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const APP_PATH = path.resolve(__dirname, 'src')
+
+module.exports = {
+  entry: APP_PATH,
+  mode: 'development',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[fullhash].js'
+  },
+
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      components: path.resolve(__dirname, './src/components/'),
+      src: path.resolve(__dirname, './src/')
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js)x?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          cacheDirectory: true
+        }
+      }
+      // {
+      //   test: /\.css$/,
+      //   use: ['style-loader', 'css-loader']
+      // }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.join(APP_PATH, 'index.html')
+    })
+  ]
+}
